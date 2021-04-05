3d5835463a7d6422e85d88253ed64a341f517a47
tomdoyle87 <36793507+tomdoyle87@users.noreply.github.com>
2021-04-05 21:34:07 +0100

    custom yesno label
    Changed dialog.select to dialog.yesno custom label

--- addon.py
+++ addon.py
@@ -7,7 +7,8 @@
 import xbmc
 import xbmcgui
 import xbmcaddon
-from subprocess import Popen
+
+
 
 # plugin constants
 __plugin__ = "configure NFS Server on OSMC "
@@ -17,23 +18,9 @@ __git_url__ = "https://github.com/tomdoyle87/osmc_nfs-server_plugin"
 __credits__ = "tomdoyle87"
 __version__ = "0.0.1"
 
-def select(heading, options, default=lambda: select_main):
-    labels = [option['label'] for option in options]
-    result = xbmcgui.Dialog().select(heading, labels)
-    selected = options[result]
-    if result == -1:
-        log_debug("using default action")
-        default()
-        return
-    selected['func'](selected)
-    selected['complete']()
 
 dialog = xbmcgui.Dialog()
-addon = xbmcaddon.Addon(id='plugin.program.OSMC_NFS_Sever')
-
-def select_main():
-    menu = [
-            {'label': 'Install NFS Server', 'func': Popen('python Kodi-nfs-server-setup.py'), 'complete': select_main},
-            {'label': 'Unstall NFS Server', 'func': Popen('Kodi-uninstall-nfs-server.py'), 'complete': select_main}
-    ]
-    select('NFS Server', menu, default=select_noop)
+if dialog.yesno('Kodi', 'Please select an option', yeslabel='Install NFS Server', nolabel='Uninstall NFS Server'):
+    xbmc.executebuiltin('RunScript(special://home/addons/plugin.program.OSMC_NFS_Sever/Kodi-nfs-server-setup.py)')
+else:
+    xbmc.executebuiltin('RunScript(special://home/addons/plugin.program.OSMC_NFS_Sever/Kodi-uninstall-nfs-server.py)')
