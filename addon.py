3c8390bde51b638ddcfbc0cd95abaea17b64fc98
tomdoyle87 <36793507+tomdoyle87@users.noreply.github.com>
2021-04-05 14:52:19 +0100

    Selection Menu Defined

--- addon.py
+++ addon.py
@@ -18,5 +18,16 @@ __git_url__ = "https://github.com/tomdoyle87/osmc_nfs-server_plugin"
 __credits__ = "tomdoyle87"
 __version__ = "0.0.1"
 
+def select(heading, options, default=lambda: select_main):
+    labels = [option['label'] for option in options]
+    result = xbmcgui.Dialog().select(heading, labels)
+    selected = options[result]
+    if result == -1:
+        log_debug("using default action")
+        default()
+        return
+    selected['func'](selected)
+    selected['complete']()
+
 dialog = xbmcgui.Dialog()
 addon = xbmcaddon.Addon(id='plugin.program.OSMC_NFS_Sever')
