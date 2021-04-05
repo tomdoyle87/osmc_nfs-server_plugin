5b69bb10c7ffd44255a7d6f807e4e2c17948e0df
tomdoyle87 <36793507+tomdoyle87@users.noreply.github.com>
2021-04-05 14:29:53 +0100

    Addon inital plugin dialog

--- addon.py
+++ addon.py
@@ -8,7 +8,6 @@ import xbmc
 import xbmcgui
 import xbmcaddon
 
-
 # plugin constants
 __plugin__ = "configure NFS Server on OSMC "
 __author__ = "tomdoyle87"
@@ -16,3 +15,6 @@ __url__ = "https://osmc.tv/"
 __git_url__ = "https://github.com/tomdoyle87/osmc_nfs-server_plugin"
 __credits__ = "tomdoyle87"
 __version__ = "0.0.1"
+
+dialog = xbmcgui.Dialog()
+addon = xbmcaddon.Addon(id='plugin.program.OSMC_NFS_Sever')
