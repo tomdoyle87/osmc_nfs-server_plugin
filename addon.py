ccb67f3e21101400e10aa114f5ca7d5770ecd6fb
tomdoyle87 <36793507+tomdoyle87@users.noreply.github.com>
2021-04-05 14:25:23 +0100

    create addon.py

---
+++ addon.py
@@ -0,0 +1,18 @@
+"""
+    Plugin to configure NFS Server on OSMC
+"""
+
+# -*- coding: UTF-8 -*-
+# main imports
+import xbmc
+import xbmcgui
+import xbmcaddon
+
+
+# plugin constants
+__plugin__ = "configure NFS Server on OSMC "
+__author__ = "tomdoyle87"
+__url__ = "https://osmc.tv/"
+__git_url__ = "https://github.com/tomdoyle87/osmc_nfs-server_plugin"
+__credits__ = "tomdoyle87"
+__version__ = "0.0.1"
